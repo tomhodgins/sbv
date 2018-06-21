bc29dff06af7632e539d417b96d13407507f1a32
Tommy Hodgins <tomhodgins@gmail.com>
2018-06-21 18:02:06 -0400

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # sbv
-Read Youtube .SBV caption files and convert to JSON, text, or HTML
+
+**Read Youtube .SBV caption files and convert to JSON, text, or HTML**
+
+## About
+
+This plugin parses Youtube's `.sbv` subtitle file format and allows you to either
+
+## Installation
+
+This software is available as a Node module from npm, as well as with pre-built binaries for windows, mac, and linux available on Github in the [releases](https://github.com/tomhodgins/sbv/releases) tab.
+
+To install sbv globally with npm, enter this into your shell:
+
+```
+npm install -g sbv
+```
+
+## Usage
+
+The following examples assume you have either installed sbv globally with npm, or are running a binary named "sbv" on your system.
+
+### json
+
+```
+sbv json <input> <output>
+
+// examples
+sbv json
+sbv json input.sbv
+sbv json input.sbv output.json
+```
+
+The first example of `sbv json` will display the help text for the JSON conversion function. If you supply the name of an `.sbv` file by itself, like `sbv json input.sbv`, it will convert that SBV file into JSON and display the resulting file in the console. If you also supply an optional output filename like `sbv json input.sbv output.json` it will convert the file to JSON and output the result as `output.json` and show nothing in the console.
+
+### text
+
+```
+sbv text <input> <output>
+
+// examples
+sbv text
+sbv text input.sbv
+sbv text input.sbv output.txt
+```
+
+The first example of `sbv text` will display the help text for the text conversion function. If you supply the name of an `.sbv` file by itself, like `sbv text input.sbv`, it will convert that SBV file into text and display the resulting file in the console. If you also supply an optional output filename like `sbv text input.sbv output.txt` it will convert the file to text and output the result as `output.txt` and show nothing in the console.
+
+### html
+
+```
+sbv html <input> <output>
+
+// examples
+sbv html
+sbv html input.sbv
+sbv html input.sbv output.html
+```
+
+The first example of `sbv html` will display the help text for the HTML conversion function. If you supply the name of an `.sbv` file by itself, like `sbv html input.sbv`, it will convert that SBV file into HTML and display the resulting file in the console. If you also supply an optional output filename like `sbv html input.sbv output.html` it will convert the file to HTML and output the result as `output.html` and show nothing in the console.
---
+++ sbv.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+
+// Read SBV caption files from YouTube
+const fs = require('fs')
+
+function read(file) {
+
+  const contents = fs.readFileSync(file).toString()
+  const data = []
+
+  contents.replace(
+    /^(\d:.*)\n((?:(?!\d+:\d+:\d+).*\n*)+)$/gm,
+    (string, time, text) =>
+      data.push({
+        time: {
+          start: time.split(',')[0],
+          end: time.split(',')[1]
+        },
+        text: text.replace(/\n/g, ' ').trim()
+      })
+  )
+
+  return data
+
+}
+
+const functions = {
+
+  json: function(input, output='filename.json') {
+
+    if (arguments.length) {
+
+      const data = JSON.stringify(read(input))
+
+      return arguments.length === 1
+             ? console.log(data)
+             : fs.writeFileSync(output, data)
+
+    } else {
+
+      return console.log('please enter a filename to read, and optionally an output filename to write')
+
+    }
+
+  },
+
+  text: function(input, output='filename.txt') {
+
+    if (arguments.length) {
+
+      const data = read(input).map(caption => `${caption.text}`).join(' ')
+
+      return arguments.length === 1
+             ? console.log(data)
+             : fs.writeFileSync(output, data)
+
+    } else {
+
+      return console.log('please enter a filename to read, and optionally an output filename to write')
+
+    }
+
+  },
+
+  html: function(input, output='filename.html') {
+
+    if (arguments.length) {
+
+      const data = '<!DOCTYPE html>\n'
+        + '<meta charset=utf-8>\n'
+        + read(input).map(caption =>
+            '<p'
+            + ' title="' + caption.time.start + ' – ' + caption.time.end + '"'
+            + ' data-start="' + caption.time.start + '"'
+            + ' data-end="' + caption.time.end + '"'
+            + '>'
+            + caption.text
+            + '</p>\n'
+          )
+          .join('')
+
+      return arguments.length === 1
+             ? console.log(data)
+             : fs.writeFileSync(output, data)
+
+    } else {
+
+      return console.log('please enter a filename to read, and optionally an output filename to write')
+
+    }
+
+  },
+
+}
+
+if (process.argv[2]) {
+
+  const [command, ...args] = [
+    ...process.argv[2].split(' '), ...process.argv.slice(3)
+  ]
+
+  if (functions[command]) {
+
+    functions[command](...args)
+
+  } else {
+
+    console.log('command not recognized')
+
+  }
+
+} else {
+
+  console.log(`\navailable output formats include:\n\n${
+    Object.entries(functions)
+      .map(entry => `- ${entry[0]}\n`)
+      .join('')
+  }`)
+
+}
